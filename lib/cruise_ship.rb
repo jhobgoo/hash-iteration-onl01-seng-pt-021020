202229989db0b1dcf7085e4672a5ac23f1b3c341
Joel Hobgood <jhobgoo@gmail.com>
2020-02-22 02:41:42 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,12 +7,17 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
- winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
+  winner
 end
- 
-winner
+
+def happy_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
 end
