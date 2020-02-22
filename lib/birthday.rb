edf7554452073db1cc606983236acec8826e8da5
Joel Hobgood <jhobgoo@gmail.com>
2020-02-22 02:48:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,10 +4,11 @@
 #   "Amanda" => 27
 # }
 
-def age_appropriate_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
+      if age <= 12
         puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-      if age > 12
+      else
         puts "You are too old for this."
       end
     end
